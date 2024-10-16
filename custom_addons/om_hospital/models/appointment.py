6d41d0db2ba5542b03a96d17f371eb805f0d841a
Dapin-Edu <161010582+Dapin-Edu@users.noreply.github.com>
2024-10-16 19:07:04 +0300

    odoo mates lesson 6

---
+++ custom_addons/om_hospital/models/appointment.py
@@ -0,0 +1,42 @@
+from odoo import api, fields, models
+
+
+class HospitalAppointment(models.Model):
+    _name = 'hospital.appointment'
+    _inherit = ['mail.thread']
+    _description = 'Hospital Appointment'
+    _rec_name  = 'patient_id'
+
+    # Model fields
+    reference = fields.Char(string="Reference", default='New', tracking=True)
+    patient_id = fields.Many2one('hospital.patient', string="Patient")
+    date_appointment = fields.Date(string="Date")
+    note = fields.Text(string="Note")
+    state = fields.Selection([
+        ('draft', 'Draft'), ('confirmed', 'Confirmed'),
+        ('ongoing', 'Ongoing'), ('done', 'Done'), ('cancel', 'Canceled')
+    ], default='draft', tracking=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            # Check for 'reference' field existence and assign a new value if it's 'New' or not present
+            if not vals.get('reference') or vals['reference'] == 'New':
+                vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or 'New'
+        return super(HospitalAppointment, self).create(vals_list)
+
+    def action_confirm(self):
+        for rec in self:
+            rec.state='confirmed'
+    def action_ongoing(self):
+        for rec in self:
+            rec.state='ongoing'
+    def action_done(self):
+        for rec in self:
+            rec.state='done'
+    def action_cancel(self):
+        for rec in self:
+            rec.state='cancel'
+
+
+
