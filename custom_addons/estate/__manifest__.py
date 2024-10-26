307312810494bfd1f8519e0a1d92a12b97fef85c
Dapin-Edu <161010582+Dapin-Edu@users.noreply.github.com>
2024-10-26 13:26:53 +0300

    added security

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,5 +2,8 @@
     "name": "Estate",
     "author": "Dapin",
     "license": "LGPL-3",
-    "version": "17.0.1.0"
+    "version": "17.0.1.0",
+    "data":[
+        "security/ir.model.access.csv"
+    ]
 }
