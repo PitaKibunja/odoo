b45a08c784a7722ea6de3328eda87f963f59c533
Dapin-Edu <161010582+Dapin-Edu@users.noreply.github.com>
2024-10-29 11:12:24 +0300

    Finished first tutorial

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -1,6 +1,12 @@
 {
     "name":"Hospital Management System",
     "author":"Dapin Technologies",
-    "license":"LGPL Version 2",
-    "version":"17.0.1.0"
+    "license":"LGPL-3",
+    "version":"17.0.1.0",
+    "data":[
+        "security/ir.model.access.csv",
+        "views/patient_views.xml",
+        "views/menu.xml"
+
+    ]
 }
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,5 +1,11 @@
 #define a model inside odoo
 from odoo import api, fields, models
 
+
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _description = 'Patient Master'
+
+    name=fields.Char(string="Name", required=True)
+    date_of_birth=fields.Date(string="DOB")
+    gender=fields.Selection([('male', 'Male'),('female','Female')], string="Gender")
