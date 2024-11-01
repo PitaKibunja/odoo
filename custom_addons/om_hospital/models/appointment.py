0d2d02c7d877836638d88052ad398779248acc71
Dapin-Edu <161010582+Dapin-Edu@users.noreply.github.com>
2024-11-01 08:12:26 +0300

    worked on creating the appointment section now entering the sequencial section

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -6,6 +6,7 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _inherit = ['mail.thread']
     _description = 'Hospital Appointment'
+    _rec_name = 'patient_id'
 
 
     patient_id = fields.Many2one('hospital.patient', string="Patient")
